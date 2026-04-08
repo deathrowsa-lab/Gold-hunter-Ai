15f9441a589550e98a65d450fb869c033607687a
deathrowsa-lab <deathrow.sa@gmail.com>
2026-04-08 18:01:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,6 @@ def home():
     return "Gold Hunter AI running"
 import os
 
-port = int(os.environ.get("PORT", 10000))
-app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
