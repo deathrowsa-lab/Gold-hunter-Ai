779ac888243416e3908ca5eb555b62d53f3c5aec
deathrowsa-lab <deathrow.sa@gmail.com>
2026-04-08 17:30:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,6 @@
-from flask import Flask, jsonify
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Gold Hunter AI Running 🚀"
-
-@app.route("/status")
-def status():
-    return jsonify({
-        "bot": "running",
-        "trades_today": 0,
-        "profit": 0
-    })
-
-app.run(host="0.0.0.0", port=10000)
+    return "Gold Hunter AI running"
